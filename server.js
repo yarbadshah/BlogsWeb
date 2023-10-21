a2d25b345e98b3edc337c45e78b9ff730e05f58b
yarbadshah <136337026+yarbadshah@users.noreply.github.com>
2023-10-21 08:05:23 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ import CategoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from "./routes/productRoutes.js";
 import cors from 'cors'
 import path from 'path'
-
+import { fileURLToPath } from "url";
 const app = express()
 
 dotenv.config()
 
 connectDB()
-
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 app.use(express.json())
 
 app.use(cors())
@@ -20,7 +21,7 @@ app.use('/api/v1/auth',authoRoutes)
 
 app.use('/api/v1/category', CategoryRoutes)
 app.use("/api/v1/product", productRoutes);
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname,'./client/build')))
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
